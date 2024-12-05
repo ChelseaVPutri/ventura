cda955ccb66e3d521a3cb68914ecf1a26119f69e
ChelseaVPutri <chelseaputri563@gmail.com>
2024-12-05 18:59:14 +0700

    ceritanya ini lagi nyoba login google

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -46,6 +46,7 @@ $routes->get('/', 'Pages::home');
 $routes->get('/login', 'Pages::login');
 $routes->get('/register', 'Pages::register');
 $routes->get('/profile', 'Pages::profile');
+$routes->get('/callback', 'Auth::callback');
 
 //rute admin
 // $routes->get('admin/login', 'Admin\AdminLogin::login');
