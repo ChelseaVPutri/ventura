fcfe4b950c9a83623cef06ccc0b7aec16c7b32d9
ChelseaVPutri <chelseaputri563@gmail.com>
2024-12-16 23:18:54 +0700

    nambahin css keranjang

--- app/Views/Pages/cart.php
+++ app/Views/Pages/cart.php
@@ -29,6 +29,67 @@
 <link rel="stylesheet" href="<?= CSS . 'keranjang.css' ?>">
 <link rel="icon" href="<?= ASSET . 'logo.png'; ?>">
 </head>
+<style>
+.cart-summary {
+    display: flex;
+    flex-direction: column;
+    padding: 15px;
+    background-color: #fff;
+    border-top: 1px solid #ddd;
+    margin-top: 10px;
+    border-radius: 8px;
+    box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+}
+
+.shop-details {
+    display: flex;
+    justify-content: space-between;
+    font-size: 16px;
+    font-weight: bold;
+    margin-bottom: 15px;
+}
+
+.summary-buttons {
+    display: flex;
+    justify-content: space-between;
+}
+
+.clear-cart,
+.checkout {
+    background-color: #EA6932;
+    color: white;
+    border: none;
+    padding: 10px 15px;
+    text-decoration: none;
+    font-size: 14px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-align: center;
+    width: 48%;
+}
+
+.clear-cart:hover,
+.checkout:hover {
+    background-color: #ff8500;
+}
+
+.remove-button {
+    background-color: #dc3545; /* Warna merah untuk tombol hapus */
+    color: white;
+    border: none;
+    padding: 5px 10px;
+    border-radius: 5px;
+    cursor: pointer;
+    text-decoration: none;
+}
+
+.remove-button:hover {
+    background-color: #c82333;
+}
+
+</style>
+
+
 <body>
 
     <header>
@@ -60,7 +121,7 @@
                         <div class="item-price">
                             <p>Rp<?= number_format($cd['price'], 0, ',', '.'); ?> x <?= $usercart[$count]['qty'] ?> =</p>
                             <p class="total-price">Rp<?php $total = $cd['price'] * $usercart[$count]['qty'];echo number_format($total, 0, ',', '.'); ?></p>
-                            <!-- <a class="remove-button" href="/uts/service/delete_cart.php?id=<?= 1//$pid?>">Hapus</a> -->
+                            <a class="remove-button" href="<? ?>">Hapus</a>
                         </div>
                     </div>
             <?php
@@ -68,6 +129,18 @@
                 endforeach;
             endif;
               ?>
+    </div>
+    
+    <div class="cart-summary">
+            <div class="shop-details">
+                <p>Total Harga:</p>
+                <!-- <p class="total-cart-price">Rp<//?= number_format($totalCart, 0, ',', '.'); ?></p> -->
+                 <p class="total-cart-price">Rp 50.000</p>
+            </div>
+            <div class="summary-buttons">
+                <a href="" class="clear-cart">Hapus Semua</a>
+                <a href="" class="checkout">Checkout</a>
+            </div>
     </div>
 </div>
 
