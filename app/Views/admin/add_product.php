c99ae2c219b55593ab63d67bea4043b1d9c8bb92
ChelseaVPutri <chelseaputri563@gmail.com>
2024-12-17 12:59:21 +0700

    ngubah css halaman atmin

--- app/Views/admin/add_product.php
+++ app/Views/admin/add_product.php
@@ -7,11 +7,61 @@
     <link rel="stylesheet" href="<?= CSS.'add-product.css'; ?>">
     <link rel="icon" href="<?php echo ASSET.'logo.png'; ?>">
 </head>
+
+<style>
+    .button-container {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      flex-wrap: wrap;
+      gap: 10px;
+      margin-top: 10px;
+    }
+
+    .button-card{
+    background-color: #ff6600; 
+    border: none;
+    color: white;
+    padding: 10px;
+    text-align: center;
+    display: inline-block;
+    font-size: 16px;
+    margin: 4px 2px;
+    cursor: pointer;
+    border-radius: 8px;
+    width: 150px;
+    opacity: 0.8;
+    transition: 0.3s;
+  }
+
+  .button-card:hover{
+    opacity: 1
+  }
+</style>
+
 <body>
-    <header>
-        <a class="back-button" href="<?= base_url('AdminPages/adminLogout') ?>" style="text-decoration: none;">&#8592;</a>
-        <h2 style="text-align: center;">Tambah Produk</h2>
+    <header style="display: flex; justify-content: space-between; align-items: center; padding: 10px;">
+        <!-- <a class="back-button" href="<//?= base_url('AdminPages/adminLogout') ?>" style="text-decoration: none;">&#8592;</a> -->
+        <h2 style="text-align: center; margin: 0; flex-grow: 1">Halaman Admin</h2>
+        <a href="<?= base_url('AdminPages/adminLogout'); ?>" style="text-decoration: none">
+            <button style="background-color: red; color: white; border: none; padding: 5px 10px; border-radius: 5px; cursor: pointer;">Logout</button>
+        </a>
     </header>
+
+    <div class="button-container">
+        <a href="#">
+            <button class="button-card">Dashboard</button>
+        </a>
+        <a href="#">
+            <button class="button-card">Daftar Produk</button>
+        </a>
+        <a href="#">
+            <button class="button-card">Tambah Produk</button>
+        </a>
+        <a href="#">
+            <button class="button-card">Daftar Pesanan</button>
+        </a>
+    </div>
 
     <div class="container">
         <?php 
